15b7c435755396c617c28cfad74d7b87919ef215
diiir <diiir727@gmail.com>
2020-04-18 16:44:02 +0300

    add version

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class Main {
 
+    @Value("${version}")
+    private String version;
+
     @GetMapping(path = "/health", produces = "application/json")
     public ResponseEntity health() {
         Response res = new Response();
         res.setResult(true);
         return ResponseEntity.ok(res);
+    }
+
+    @GetMapping(path = "/version", produces = "application/json")
+    public ResponseEntity version() {
+        return ResponseEntity.ok(version);
     }
 
 }
